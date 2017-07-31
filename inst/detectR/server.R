d8ac54ba8f0135ce0609c5a85d945b2f33185f64
Joel Gombin <joel.gombin@gmail.com>
2017-07-31 07:18:53 +0200

    persistent selection \o/

--- inst/detectR/server.R
+++ inst/detectR/server.R
@@ -1,13 +1,14 @@
 
 shinyServer(function(input, output, session) {
 
-
-  observe({
-    updateSelectizeInput(session,
+  observeEvent({input$hypotheses
+                input$revues
+                input$books}, 
+               {
+                updateSelectizeInput(session,
                            "publications",
-                           choices = publis[c(input$hypotheses, input$revues, input$books)]
-                           )
-#                           selected = publications_selected())
+                           choices = publis[c(input$hypotheses, input$revues, input$books)],
+                           selected = input$publications)
   })
   
   
