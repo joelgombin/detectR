8d5b9ab75be0e4d0ad4589f92d541cf2f72b0627
Joel Gombin <joel.gombin@gmail.com>
2017-07-31 15:33:42 +0200

    if no selection all urls

--- inst/detectR/server.R
+++ inst/detectR/server.R
@@ -12,17 +12,27 @@ shinyServer(function(input, output, session) {
   })
 
   output$urls <- DT::renderDataTable({
-    anomaly_detection$url_prediction_anomalies %>% 
-      right_join(
-        urls %>% 
-          filter(site_url %in% input$publications),
-        by = c("url" = "id")
-      ) %>% 
-      left_join(publications, by = c("site_url", "platform")) %>% 
-      select(platform, site_titre, naked_titre, url, fit_sigma_ratio) %>% 
-      arrange(desc(fit_sigma_ratio)) %>% 
-      mutate(fit_sigma_ratio = round(fit_sigma_ratio)) %>% 
-      rename(Plateforme = platform, Publication = site_titre, Document = naked_titre, URL = url, intensité = fit_sigma_ratio)
+    if (is_null(input$publications)) {
+      anomaly_detection$url_prediction_anomalies %>% 
+        right_join(urls, by = c("url" = "id")) %>% 
+        left_join(publications, by = c("site_url", "platform")) %>% 
+        select(platform, site_titre, naked_titre, url, fit_sigma_ratio) %>% 
+        arrange(desc(fit_sigma_ratio)) %>% 
+        mutate(fit_sigma_ratio = round(fit_sigma_ratio)) %>% 
+        rename(Plateforme = platform, Publication = site_titre, Document = naked_titre, URL = url, intensité = fit_sigma_ratio)
+    } else {
+      anomaly_detection$url_prediction_anomalies %>% 
+        right_join(
+          urls %>% 
+            filter(site_url %in% input$publications),
+          by = c("url" = "id")
+        ) %>% 
+        left_join(publications, by = c("site_url", "platform")) %>% 
+        select(platform, site_titre, naked_titre, url, fit_sigma_ratio) %>% 
+        arrange(desc(fit_sigma_ratio)) %>% 
+        mutate(fit_sigma_ratio = round(fit_sigma_ratio)) %>% 
+        rename(Plateforme = platform, Publication = site_titre, Document = naked_titre, URL = url, intensité = fit_sigma_ratio)
+    }
   },
   server = TRUE,
   selection = "single")
