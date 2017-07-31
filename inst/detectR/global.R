dd2c4a70d80d134893bcc40bd99736417af06b3d
Joel Gombin <joel.gombin@gmail.com>
2017-07-31 06:21:55 +0200

    read data from solr rather than static file memoise this to speed up launch of the app

--- inst/detectR/global.R
+++ inst/detectR/global.R
@@ -1,42 +1,51 @@
 library(urltools)
+library(solrium)
+library(memoise)
 library(tidyverse)
-livres <- read_csv("http://www.openedition.org/index.html?page=coverage&pubtype=livre&export=csv",
-                   col_types = cols(
-                     Title = col_character(),
-                     `ISBN (print format)` = col_character(),
-                     `ISBN (electronic format)` = col_character(),
-                     `Electronic Publication Date` = col_date(format = ""),
-                     Url = col_character(),
-                     Authors = col_character(),
-                     Publisher = col_character(),
-                     `Print Publication Date (Year)` = col_integer(),
-                     Abstract = col_character(),
-                     `Access policy` = col_character(),
-                     `Main language (ISO 639-1)` = col_character()
-                   ))
-revues <- read_csv("http://www.openedition.org/index.html?page=coverage&pubtype=revue&export=csv")
-carnets <- read_csv("http://www.openedition.org/index.html?page=coverage&pubtype=carnet&export=csv")
-livres <- livres %>%
-  mutate(Title = stringr::str_replace_all(Title, "'|’", "'"))
-revues <- revues %>%
-  mutate(Title = stringr::str_replace_all(Title, "'|’", "'"))
-carnets <- carnets %>%
-  mutate(Title = stringr::str_replace_all(Title, "'|’", "'"))
+solr_connect('http://147.94.102.65:8983/solr/documents/select')
 
-publications <- bind_rows(hypotheses = carnets %>% select(Title, Url),
-                          books = livres %>%  select(Title, Url),
-                          revues = revues %>% select(Title, Url), .id = "plateforme") %>%
-  mutate(Url = map(Url, url_parse)) %>%
-  unnest(Url) %>%
-  mutate(url = case_when(plateforme %in% c("carnets", "revues") ~ domain,
-                         TRUE ~ paste0(domain, "/", path))) %>%
-  select(plateforme, Title, url)
+fc <- cache_filesystem("~/.Rcache")
+# memoisation of the solr call. Reset it every day.
+m_solr_facet <- memoise(solr_facet, cache = fc, ~timeout(60*60*24))
+m_solr_search <- memoise(solr_search, cache = fc)
+
+publications <- m_solr_facet(q = "*:*", facet.pivot = c("platform", "site_url", "site_titre"), facet.limit = -1)$facet_pivot$`platform,site_url,site_titre` %>% unnest()
 
 load("./data/anomaly_detection.Rdata")
-urls <- anomaly_detection$url_prediction_anomalies %>%
-  group_by(url) %>%
-  arrange(-fit_sigma_ratio) %>%
-  slice(1) %>%
-  ungroup %>%
-  arrange(-fit_sigma_ratio)
 
+load_urls <- function() {
+  urls <- anomaly_detection$url_prediction_anomalies %>%
+    group_by(url) %>%
+    arrange(-fit_sigma_ratio) %>%
+    slice(1) %>%
+    ungroup %>%
+    arrange(-fit_sigma_ratio) %>% 
+    mutate(url = if_else(stringr::str_detect(url, "http://"), url, paste0("http://", url)))
+  map2_df(seq(1, length(urls$url), by = 100), seq(100, length(urls$url)+100, by = 100), ~ m_solr_search(q = paste0('id:(', paste0('"', urls$url[.x:min(.y, length(urls$url))], '"', collapse = ","), ")"), fl=c("id", "platform", "site_url", "naked_titre"), wt = "json", rows = 101))
+}
+
+m_load_urls <- memoise(load_urls, cache = fc, ~timeout(60*60*24))
+
+urls <- m_load_urls()
+
+publis <- list(
+  "Hypothèses" = publications %>% 
+    filter(platform %in% "Hypotheses.org") %>% 
+    pull(site_url),
+  "Revues" = publications %>% 
+    filter(platform %in% "Revues.org") %>% 
+    pull(site_url),
+  "Books" = publications %>% 
+    filter(platform %in% "OpenEdition Books") %>% 
+    pull(site_url)
+)
+
+names(publis$Hypothèses) <- publications %>% 
+  filter(platform %in% "Hypotheses.org") %>% 
+  pull(site_titre)
+names(publis$Revues) <- publications %>% 
+  filter(platform %in% "Revues.org") %>% 
+  pull(site_titre)
+names(publis$Books) <- publications %>% 
+  filter(platform %in% "OpenEdition Books") %>% 
+  pull(site_titre)
