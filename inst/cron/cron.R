4a0ecc673841ea0ae76731c45900f13b2c6cd69e
Joel Gombin <joel.gombin@gmail.com>
2017-08-23 16:19:24 +0200

    function to be executed by a cron job

---
+++ inst/cron/cron.R
@@ -0,0 +1,26 @@
+library(pool)
+library(MonetDBLite)
+library(dplyr)
+library(detectR)
+
+mysql <- config::get("mysql", file = "~/detectR/inst/cron/config.yml")
+monetdb <- config::get("monetdb", file = "~/detectR/inst/cron/config.yml")
+
+conn1 <- dbPool(drv = eval(parse(text = mysql$drv)),
+                host = mysql$host,
+                user = mysql$user,
+                pass = mysql$pass,
+                dbname = mysql$dbname,
+                port = mysql$port)
+conn2 <- dbPool(drv = eval(parse(text = monetdb$drv)),
+                embedded = monetdb$embedded)
+
+all_actions <- tbl(conn2, "all_actions")
+
+from <- all_actions %>%
+  select(idvisit) %>% 
+  arrange(desc(idvisit)) %>% 
+  collect(n = 1) %>% 
+  pull(idvisit) + 1
+
+extract_and_load(conn1, conn2, from = from)
