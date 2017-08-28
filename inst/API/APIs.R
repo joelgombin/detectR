30c51a8a3359872a7a0d55ae250000ee32f2c8c5
Joel Gombin <joel.gombin@gmail.com>
2017-08-28 02:16:27 +0200

    apply changes of `get_visits` to the API

--- inst/API/APIs.R
+++ inst/API/APIs.R
@@ -1,5 +1,5 @@
 #* @get /get_visits
-get_visits <- function(visited_url, from = "2017-01-01", to = "2017-08-01") {
+get_visits <- function(visited_urls, from = "2017-01-01", to = "2017-08-01") {
   library(dplyr)
   monetdb_con <- DBI::dbConnect(MonetDBLite::monetdblite(), "/data/monetdb")
   tbl_all_actions <- tbl(monetdb_con, "all_actions")
@@ -7,7 +7,7 @@ get_visits <- function(visited_url, from = "2017-01-01", to = "2017-08-01") {
   from <- lubridate::ymd(from)
   to <- lubridate::ymd(to)
   
-  detectR::get_visits(visited_url, from, to, tbl_all_actions)
+  detectR::get_visits(tbl_all_actions, visited_urls, from, to)
 }
 
 #* @assets /data/dashboards /dashboards
