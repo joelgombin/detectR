a53c79985bfda3e1368b211b129ab7e26dfa4b4f
Joel Gombin <joel.gombin@gmail.com>
2017-08-28 02:15:19 +0200

    allow to group by multiple URLs + backend first argument

--- R/get_visits.R
+++ R/get_visits.R
@@ -1,16 +1,24 @@
 #' Get number of visits for a given URL
 #' @importFrom magrittr %>% 
 #' @export
-get_visits <- function(visited_url, from, to, backend) {
-  backend %>% 
-    select(idvisit, url, date) %>% 
-    filter(url == visited_url) %>% 
+#' @param backend table (usually a SQL table) friom which the data is agregated 
+#' @param visited_urls a vector of urls 
+get_visits <- function(backend, visited_urls = NULL, from = Sys.Date(), to = Sys.Date()) {
+  tmp_df <- backend %>% 
+    select(idvisit, url, date)
+  if (!is.null(visited_urls)) {
+    tmp_df <- tmp_df %>% 
+      filter(url %in% visited_urls)
+  } 
+  tmp_df %>% 
+    filter(url != "http://") %>% 
+    filter(!is.na(url)) %>% 
     filter(date >= from,
            date <= to) %>% 
-    select(-url) %>% 
     collect %>% 
+    group_by(url) %>% 
     distinct(idvisit, .keep_all = TRUE) %>% 
-    group_by(date) %>% 
+    group_by(date, url) %>% 
     summarise(n = n()) %>% 
     ungroup
 }
