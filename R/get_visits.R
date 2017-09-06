2a3498d27df001a2b459001abb9e35a99b0e49b7
Joel Gombin <joel.gombin@gmail.com>
2017-09-06 02:03:37 +0200

    in `get_visits`, separate the backend and the table make sure functions are linked to their home package details about the output write request in SQL

--- R/get_visits.R
+++ R/get_visits.R
@@ -1,24 +1,29 @@
 #' Get number of visits for a given URL
 #' @importFrom magrittr %>% 
 #' @export
-#' @param backend table (usually a SQL table) friom which the data is agregated 
+#' @param conn a connection to the database from which the data is retrieved
+#' @param table the table name from which the data is agregated 
 #' @param visited_urls a vector of urls 
-get_visits <- function(backend, visited_urls = NULL, from = Sys.Date(), to = Sys.Date()) {
-  tmp_df <- backend %>% 
-    select(idvisit, url, date)
-  if (!is.null(visited_urls)) {
-    tmp_df <- tmp_df %>% 
-      filter(url %in% visited_urls)
-  } 
+#' @details This function only retains one hit per visit (in the meaning of piwik). Only non-zero values are kept; in order to show zero values, please use `tidyr::fill`.
+get_visits <- function(conn, table = "all_actions", visited_urls = NULL, from = Sys.Date(), to = Sys.Date()) {
+  
+  if (is.null(visited_urls)) {
+    request <- paste0('SELECT idvisit, url, date FROM ', table,' WHERE "date" >= \'', lubridate::ymd("2017-01-01"), '\' AND "date" <= \'', Sys.Date(), '\'')
+  } else {
+    request <- paste0('SELECT idvisit, url, date FROM ', table, ' WHERE url IN (\'', paste0(visited_urls, collapse = "\', \'"), '\') AND "date" >= \'', lubridate::ymd("2017-01-01"), '\' AND "date" <= \'', Sys.Date(), '\'')
+  }
+  
+#  cat(request) for debug
+  
+  tmp_df <- DBI::dbGetQuery(conn, request)
+  
   tmp_df %>% 
-    filter(url != "http://") %>% 
-    filter(!is.na(url)) %>% 
-    filter(date >= from,
-           date <= to) %>% 
-    collect %>% 
-    group_by(url) %>% 
-    distinct(idvisit, .keep_all = TRUE) %>% 
-    group_by(date, url) %>% 
-    summarise(n = n()) %>% 
-    ungroup
+    dplyr::filter(validate_url(url, logical = TRUE)) %>% 
+    dplyr::filter(date >= from, date <= to) %>% 
+    dplyr::group_by(url) %>% 
+    dplyr::distinct(idvisit, .keep_all = TRUE) %>% 
+    dplyr::group_by(date, url) %>% 
+    dplyr::summarise(n = n()) %>% 
+    dplyr::ungroup()
+    
 }
