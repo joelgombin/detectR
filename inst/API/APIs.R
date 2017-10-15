cfc1a2e8d816791cb81f8fb78d5398b6aaa8fdd4
Joel Gombin <joel.gombin@gmail.com>
2017-10-15 03:02:27 +0200

    use pool in the API

--- inst/API/APIs.R
+++ inst/API/APIs.R
@@ -1,13 +1,15 @@
 #* @get /get_visits
 get_visits <- function(visited_urls, from = "2017-01-01", to = "2017-08-01") {
   library(dplyr)
-  monetdb_con <- DBI::dbConnect(MonetDBLite::monetdblite(), "/data/monetdb")
-  tbl_all_actions <- tbl(monetdb_con, "all_actions")
+  monetdb <- config::get("monetdb", file = "~/detectR/inst/cron/config.yml")
+  conn <- dbPool(drv = eval(parse(text = monetdb$drv)),
+                  embedded = monetdb$embedded)
+  tbl_all_actions <- tbl(conn2, "all_actions")
   
   from <- lubridate::ymd(from)
   to <- lubridate::ymd(to)
   
-  detectR::get_visits(tbl_all_actions, visited_urls, from, to)
+  detectR::get_visits(conn, "all_actions", visited_urls, from, to)
 }
 
 #* @assets /data/dashboards /dashboards
