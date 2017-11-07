35829aeb1c1383a44ecbaf523a682da68f3c3ac4
Joel Gombin <joel.gombin@gmail.com>
2017-11-07 17:27:30 +0100

    on.exit approach

--- inst/API/APIs.R
+++ inst/API/APIs.R
@@ -3,6 +3,7 @@ library(tidyverse)
 #* @get /get_visits
 get_visits <- function(visited_urls, from = "2017-01-01", to = "2017-08-01") {
   monetdb <- config::get("monetdb", file = "~/detectR/inst/cron/config.yml")
+  on.exit({pool::poolClose(conn)})
   conn <- pool::dbPool(drv = eval(parse(text = monetdb$drv)),
                   embedded = monetdb$embedded)
   tbl_all_actions <- dplyr::tbl(conn, "all_actions")
@@ -11,17 +12,17 @@ get_visits <- function(visited_urls, from = "2017-01-01", to = "2017-08-01") {
   to <- lubridate::ymd(to)
   
   tmp <- detectR::get_visits(conn, "all_actions", visited_urls, from, to)
-  pool::poolClose(conn)
   return(tmp)
 }
 
 #* @get /get_outliers
 get_outliers <- function(from = "2017-01-01", to = "2017-08-01") {
   monetdb <- config::get("monetdb", file = "~/detectR/inst/cron/config.yml")
+  on.exit({pool::poolClose(conn)})
   conn <- pool::dbPool(drv = eval(parse(text = monetdb$drv)),
                  embedded = monetdb$embedded)
+  
   tmp <- dplyr::tbl(conn, "url_prediction_anomalies") %>% collect()
-  pool::poolClose(conn)
   return(tmp)
 }
 
