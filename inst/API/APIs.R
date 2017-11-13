b9924c6988e6e198d8a45ca098b1eb14569de118
Joel Gombin <joel.gombin@gmail.com>
2017-11-13 08:08:43 +0100

    utilisation de la base de sauvegarde (pour éviter conflits d'usage la nuit)

--- inst/API/APIs.R
+++ inst/API/APIs.R
@@ -2,28 +2,29 @@ library(tidyverse)
 
 #* @get /get_visits
 get_visits <- function(visited_urls = NULL, from = "2017-01-01", to = "2017-08-01") {
-  monetdb <- config::get("monetdb", file = "~/detectR/inst/cron/config.yml")
+  monetdb <- config::get("monetdb", file = "~/detectR/inst/API/config.yml")
   on.exit({pool::poolClose(conn)})
   conn <- pool::dbPool(drv = eval(parse(text = monetdb$drv)),
                   embedded = monetdb$embedded)
-  tbl_all_actions <- dplyr::tbl(conn, "all_actions")
-  
+
   from <- lubridate::ymd(from)
   to <- lubridate::ymd(to)
   
-  tmp <- detectR::get_visits(conn, "all_actions", visited_urls, from, to)
+  tmp <- detectR::get_visits(conn, "visites_par_jour", visited_urls, from, to)
   return(tmp)
 }
 
 #* @get /get_outliers
 get_outliers <- function(from = "2017-01-01", to = "2017-08-01") {
-  monetdb <- config::get("monetdb", file = "~/detectR/inst/cron/config.yml")
+  monetdb <- config::get("monetdb", file = "~/detectR/inst/API/config.yml")
   on.exit({pool::poolClose(conn)})
   conn <- pool::dbPool(drv = eval(parse(text = monetdb$drv)),
                  embedded = monetdb$embedded)
-  
-  tmp <- dplyr::tbl(conn, "url_prediction_anomalies") %>% collect()
+  date <- "date"
+  request <- glue::glue_sql("SELECT * FROM url_prediction_anomalies WHERE {`date`} >= {from} AND {`date`} <= {to}", .con = conn)
+  tmp <- tibble::as_tibble(DBI::dbGetQuery(conn, request))
   return(tmp)
+#  pool::poolClose(conn)
 }
 
 #* @assets /data/dashboards /dashboards
